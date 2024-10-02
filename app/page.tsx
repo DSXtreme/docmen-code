c2cd659ebb190a16803b148df3d35ca7ac219d17
Debmalya De Sarkar <debmalya.desarkar@gmail.com>
2024-10-02 12:05:28 +0530

    Code sgement improved

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,13 @@
 /** @jsxImportSource @emotion/react */
 import { css, SerializedStyles } from "@emotion/react";
 
-import { useEffect, useState } from "react";
-import hljs from "highlight.js";
-import "highlight.js/styles/monokai.css";
+import { useEffect, useRef, useState } from "react";
+
+import "prismjs/themes/prism-tomorrow.css";
+import Prism from "prismjs";
+import "prismjs/components/prism-javascript"; // Load languages as needed
+import "prismjs/components/prism-css";
+import "prismjs/components/prism-markdown"; // Example
 
 // Components
 import Markdown from "react-markdown";
@@ -19,15 +23,18 @@ const markdown =
     '## User Model with Mongoose\n\nThis code defines a Mongoose model for a "User" document in a MongoDB database.\n\n**1. Import necessary modules:**\n\n```javascript\nimport mongoose, { Schema, Types } from "mongoose";\n```\n\n**2. Define the raw document interface:**\n\n```javascript\ninterface UserType {\n    name: string;\n    email: string;\n    password: string;\n    projects: Types.ObjectId;\n}\n```\n\nThis interface defines the data types of the fields in the "User" document. It uses primitive types like `string` and `Types.ObjectId` for fields that will be directly stored in MongoDB. \n\n**3. Define the Mongoose schema:**\n\n```javascript\nconst userSchema = new Schema<UserType>({\n    name: { type: String, required: true },\n    email: { type: String, required: true },\n    password: {\n        type: String,\n        required: true,\n    },\n    projects: [\n        {\n            type: Schema.Types.ObjectId,\n            ref: "projects",\n        },\n    ],\n});\n```\n\nThis schema defines the structure of the "User" document. It specifies the type of each field, any validation rules (like `required`), and relationships to other models. In this case, the `projects` field is an array of `Types.ObjectId` references to documents in the "projects" collection.\n\n**4. Create the Mongoose model:**\n\n```javascript\nconst User = mongoose.model("users", userSchema);\n```\n\nThis line creates a Mongoose model called "User" based on the `userSchema`. This model provides methods for interacting with the database, such as creating, reading, updating, and deleting documents.\n\n **5. Export the model:**\n\n```javascript\nexport default User;\n```\n\nThis line makes the "User" model available for use in other parts of the application.\n\n**Key points:**\n\n- The model defines the structure and validation rules for "User" documents.\n- It uses `Types.ObjectId` to establish relationships with other models, like "projects".\n- The model provides methods for interacting with the database.\n\n**Further improvements:**\n\n- Add validation rules for email and password fields.\n- Consider using a password hashing library to secure passwords.\n- Implement additional features like user authentication and authorization.\n';
 
 export default function Home() {
+    // ref
+    const codeRef = useRef<HTMLElement>();
+
     // local state
     const [input, setInput] = useState<string>("");
-    const [markdown, setMarkdown] = useState<string>("");
+    // const [markdown, setMarkdown] = useState<string>("");
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
     // Initializeing code heighlighter
     useEffect(() => {
-        hljs.highlightAll();
-    }, [markdown]);
+        Prism.highlightAll();
+    }, []);
 
     // Converting to markdown and downlaod .md
     const downloadMarkdown = () => {
@@ -60,7 +67,7 @@ export default function Home() {
 
             const data = res.data;
 
-            setMarkdown(data.markdown_data);
+            // setMarkdown(data.markdown_data);
 
             setIsLoading(false);
         }
@@ -84,7 +91,7 @@ export default function Home() {
                 </div>
                 <div css={rght_pannel}>
                     {/* Hint */}
-                    {isLoading ? (
+                    {/* {isLoading ? (
                         <div css={right_panel}>
                             <Loading />
                         </div>
@@ -92,19 +99,21 @@ export default function Home() {
                         <div css={right_panel}>
                             Paste your code to generate.
                         </div>
-                    ) : (
-                        <Markdown
-                            components={{
-                                code({ children }) {
-                                    return (
-                                        <code css={code_block}>{children}</code>
-                                    );
-                                },
-                            }}
-                        >
-                            {markdown}
-                        </Markdown>
-                    )}
+                    ) : ( */}
+                    <Markdown
+                        components={{
+                            code({ children }) {
+                                return (
+                                    <code css={code_block} className={`language-javascript`}>
+                                        {String(children)}
+                                    </code>
+                                );
+                            },
+                        }}
+                    >
+                        {markdown}
+                    </Markdown>
+                    {/* )} */}
                 </div>
             </div>
         )
