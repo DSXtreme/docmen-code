a5bb0ac56f88dd2a614d2740fbe5f4ca6bce8e57
Debmalya De Sarkar <debmalya.desarkar@gmail.com>
2024-10-06 20:36:53 +0530

    icon and seo imporved

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,26 @@
 "use client";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { Metadata } from "next";
+
+import Image from "next/image";
 
 type LayoutProps = {
     children: React.ReactNode;
 };
 
+
 const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
     return (
         <div css={layout_container}>
-            <nav></nav>
+            <nav>
+                <Image
+                    src={"/logo_d_code.png"}
+                    width={120}
+                    height={38}
+                    alt="DCode"
+                />
+            </nav>
             <main>{children}</main>
         </div>
     );
@@ -21,12 +32,15 @@ const layout_container = css`
     min-width: 600px;
     nav {
         width: 100%;
-        height: 45px;
+        height: 50px;
         background: var(--color-surface-200);
         position: fixed;
         top: 0;
         left: 0;
         z-index: 500;
+        display: flex;
+        align-items: center;
+        padding: 0 1rem;
     }
     main {
         width: 100%;
